b0b74a616696ec4ad0b12be12406986cb1f8f35f
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-14 13:58:09 -0400

    pwd lab

--- server/app.py
+++ server/app.py
@@ -18,26 +18,69 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        username = request.get_json()['username']
+        # user = User.query.filter(User.username == username)
 
+        password = request.get_json()['password']
+        if username and password:
+            new_user = User(
+                username=username
+            )
+            new_user.password_hash = password
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201
+        else:
+            return {},204
+    
+        # return {'error': 'Invalid username or password'}, 401
+
+        
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        password = request.get_json()['password']
+
+
+        # users pwd is set by calling user.pawd = "new_pwd"
+        # instead of pwd = user.pwd , here we authenticate by using bcrypt checking pwd = stored pwd hash
+        if user.authenticate(password):
+        
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+ 
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='checksession')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
